b4ea50aad1a19e6ea0505632724c12831dc86e07
sungjine456 <sungjine456@gmail.com>
2016-05-21 10:02:37 +0900

    비밀번호 암호화

--- src/main/java/kr/co/person/service/impl/UserServiceImpl.java
+++ src/main/java/kr/co/person/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package kr.co.person.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kr.co.person.common.Common;
 import kr.co.person.domain.User;
 import kr.co.person.repository.UserRepository;
 import kr.co.person.service.UserService;
@@ -11,9 +12,14 @@ import kr.co.person.service.UserService;
 public class UserServiceImpl implements UserService {
 	
 	@Autowired UserRepository userRepository;
+	@Autowired Common common;
 	
 	@Override
 	public void create(User user){
+		String password = user.getPassword();
+		password = common.passwordEncryptiop(password);
+		user.setPassword(password);
+		
 		userRepository.save(user);
 	}
 }
