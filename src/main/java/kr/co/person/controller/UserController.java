eb2af5bb6a8a515bf386e8ec2c3d5695602d365e
sungjine456 <sungjine456@gmail.com>
2016-06-12 15:56:37 +0900

    암호화 키 값을 파일로 분리

--- src/main/java/kr/co/person/controller/UserController.java
+++ src/main/java/kr/co/person/controller/UserController.java
@@ -12,6 +12,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +29,8 @@ import kr.co.person.service.UserService;
 @Controller
 public class UserController {
 	private static final Logger log = LoggerFactory.getLogger(UserController.class);
-	private static final String ENCRYPTION_KEY = "personProjectByJin";
+	@Value("${keyValue}")
+	private String ENCRYPTION_KEY;
 	
 	@Autowired 
 	private UserService userService;
