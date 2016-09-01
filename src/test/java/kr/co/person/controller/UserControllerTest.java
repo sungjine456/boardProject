4072bf9e1f8e46095e58a2bf2a9a97d1dd19f4ee
sungjine456 <sungjine456@gmail.com>
2016-09-01 23:08:07 +0900

    controller test 중..

--- src/test/java/kr/co/person/controller/UserControllerTest.java
+++ src/test/java/kr/co/person/controller/UserControllerTest.java
@@ -1,26 +1,44 @@
 package kr.co.person.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import org.junit.*;
+import java.io.FileInputStream;
+
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(MockitoJUnitRunner.class)
+import kr.co.person.BoardProjectApplication;
+import kr.co.person.domain.User;
+import kr.co.person.service.UserService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BoardProjectApplication.class)
+@WebAppConfiguration
 public class UserControllerTest {
 	
+	@Mock private UserService userService;
 	@InjectMocks private UserController userController;
 	@Autowired private WebApplicationContext wac;
 	private MockMvc mock;
 	 
     @Before
     public void setUp() throws Exception {
+    	MockitoAnnotations.initMocks(this);
     	this.mock = MockMvcBuilders.webAppContextSetup(wac).build();
     }
 	
@@ -28,14 +46,37 @@ public class UserControllerTest {
     public void testJoinView() throws Exception {
     	mock.perform(get("/join"))
     		.andExpect(status().isOk())
-    		.andExpect(view().name("joinView"));
+    		.andExpect(view().name("view/user/join"));
+    }
+    
+    @Test
+    public void testJoin() throws Exception {
+    	FileInputStream fis = new FileInputStream("C:/boardProject/img/user/none.png");
+        MockMultipartFile file = new MockMultipartFile("file", "none.png", "multipart/form-data", fis);
+        
+    	DateTime date = new DateTime();
+    	mock.perform(
+			post("/join")
+				.requestAttr("user", new User("test", "test@naver.com", "123456", "test", "", date, date))
+				.requestAttr("file", file)
+				.contentType(MediaType.MULTIPART_FORM_DATA))
+    		.andExpect(status().isOk())
+    		.andExpect(view().name("view/user/join"));
     }
     
     @Test
     public void testIdCheck() throws Exception {
     	mock.perform(
-    			get("/join/idCheck")
-    				.param("id", "test"))
-    	.andExpect(status().isOk());
+    		post("/join/idCheck")
+    			.accept(MediaType.APPLICATION_JSON_VALUE))
+    		.andExpect(status().isOk());
+    }
+
+    @Test
+    public void testEmailCheck() throws Exception {
+    	mock.perform(
+    		post("/join/emailCheck")
+    			.accept(MediaType.APPLICATION_JSON_VALUE))
+    		.andExpect(status().isOk());
     }
 }
