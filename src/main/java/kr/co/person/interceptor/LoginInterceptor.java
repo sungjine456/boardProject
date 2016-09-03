ecc06e6c827802acb47511c0efa4775ad0ef8210
sungjine456 <sungjine456@gmail.com>
2016-09-03 20:38:28 +0900

    session에 저장된 정보와 db에 저장된 정보가 일치하는지 interceptor를 통한 체크

--- src/main/java/kr/co/person/interceptor/LoginInterceptor.java
+++ src/main/java/kr/co/person/interceptor/LoginInterceptor.java
@@ -4,18 +4,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import kr.co.person.common.IsValid;
+import kr.co.person.domain.User;
+import kr.co.person.service.UserService;
+
 public class LoginInterceptor extends HandlerInterceptorAdapter {
+	
+	@Autowired private UserService userService;
 	static final Logger log = LoggerFactory.getLogger(LoginInterceptor.class);
     
     @Override
     public boolean preHandle(HttpServletRequest req, HttpServletResponse res, Object handler) throws Exception {
     	log.info("interceptor excute");
     	String url = req.getRequestURI();
-    	log.info("interceptor url : " + url);
     	if(url.equals("/testest")){
     		return true;
     	}
@@ -26,6 +33,17 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
     	HttpSession session = req.getSession();
 		if(session.getAttribute("loginYn") == null || session.getAttribute("loginYn").equals("N")){
 			log.info("interceptor login required excute");
+			res.sendRedirect("/interceptorView");
+			return false;
+		}
+		int idx = (int)session.getAttribute("idx");
+		String id = (String)session.getAttribute("id");
+		String name = (String)session.getAttribute("name");
+		String email = (String)session.getAttribute("email");
+		User user = userService.findUserForIdx(idx);
+		if(IsValid.isNotValidObjects(user) || !StringUtils.equals(id, user.getId())
+				|| !StringUtils.equals(name, user.getName()) || !StringUtils.equals(email, user.getEmail())){
+			log.info("interceptor login no metch user excute");
 			res.sendRedirect("/interceptorView");
 			return false;
 		}
